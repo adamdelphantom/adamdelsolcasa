e58f36a962c82df2e2788b0190efba1bd9eb93de
Adam Del Sol <adamdelsolrun@gmail.com>
2023-06-19 19:57:45 -0400

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import Navbar from './Navbar'
 
 function App() {
   return (
     <>
-      <h1>Casa Del Sol</h1>
       <Navbar/>
+      <h1>Casa Del Sol</h1>
     </>
   )
 }
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,20 +1,25 @@
+import "./Navbar.css";
+
 function Navbar() {
-    return (
-        <nav>
-            <ul>
-                <li style={{ margin: "0 5px" }}>
-                    <a href="https://github.com/adamdelsol" target="_blank">
-                        <i className="fab fa-github"></i>
-                    </a>
-                </li>
-                <li style={{ margin: "0 5px" }}>
-                    <a href="https://www.twitter.com/adamdelsol" target="_blank">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <a href="/">
+        <img src="src/assets/oasys_vest_oogy.PNG" alt="logo" />
+      </a>
+      <ul>
+        <li style={{ margin: "0 5px" }}>
+          <a href="https://github.com/adamdelsol" target="_blank">
+            <i className="fab fa-github"></i>
+          </a>
+        </li>
+        <li style={{ margin: "0 5px" }}>
+          <a href="https://www.twitter.com/adamdelsol" target="_blank">
+            <i className="fab fa-twitter"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
