7f81e54e4be766a24b531fb7b5cebfb3bc388ddf
Adam Del Sol <adamdelsolrun@gmail.com>
2023-06-19 19:13:56 -0400

    app setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+  return (
+    <>
+      <h1>Casa Del Sol</h1>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.tsx
@@ -0,0 +1,20 @@
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li style={{ margin: "0 5px" }}>
+                    <a href="https://github.com/adamdelsol" target="_blank">
+                        <i className="fab fa-github"></i>
+                    </a>
+                </li>
+                <li style={{ margin: "0 5px" }}>
+                    <a href="https://www.twitter.com/adamdelsol" target="_blank">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
